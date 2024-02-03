5cf3b8d8992cb256f158f7036d61d79f5f1ef25a
Dragomira <132193889+dragomirak@users.noreply.github.com>
2024-02-03 13:57:31 +0200

    Add project files.

---
+++ BoxOfT/Box.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace BoxOfT;
+
+public class Box<T>
+{
+    private List<T> data;
+
+    public Box()
+    {
+        this.data = new List<T>();
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.data.Count;
+        }
+    }
+
+    public void Add(T element)
+    {
+        this.data.Add(element);
+    }
+
+    public T Remove()
+    {
+        if (this.data.Count == 0)
+        {
+            throw new InvalidOperationException("The box is empty.");
+        }
+        var lastElement = this.data.Last();
+        this.data.RemoveAt(this.data.Count - 1);
+
+        return lastElement;
+    }
+}
---
+++ GenericScale/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericScale;
+
+public class StartUp
+{
+    static void Main()
+    {
+        var scale = new EqualityScale<int>(6, 6);
+        var result = scale.AreEqual();
+        Console.WriteLine(result);
+    }
+}
